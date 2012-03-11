1ad4b8658772b9caf8c956823137aed7c0333e10
Tobias Ottenweller <mail@ottenweller.net>
2012-03-11 15:33:34 +0100

    added world to info command

--- src/org/mcteam/ancientgates/commands/CommandInfo.java
+++ src/org/mcteam/ancientgates/commands/CommandInfo.java
@@ -21,12 +21,12 @@ public class CommandInfo extends BaseCommand
 		sendMessage(ChatColor.LIGHT_PURPLE + "Information about " + ChatColor.WHITE + gate.getId() + ChatColor.LIGHT_PURPLE + ":");
 		
 		if (gate.getFrom() != null)
-			sendMessage(ChatColor.GREEN + "'from' location:       " + ChatColor.YELLOW + "( " + gate.getFrom().getBlockX() + " | " + gate.getFrom().getBlockY() + " | " + gate.getFrom().getBlockZ() + " )");
+			sendMessage(ChatColor.GREEN + "'from' location:       " + ChatColor.YELLOW + "( " + gate.getFrom().getBlockX() + " | " + gate.getFrom().getBlockY() + " | " + gate.getFrom().getBlockZ() + " ) in " + gate.getFrom().getWorld().getName());
 		else
 			sendMessage(ChatColor.GREEN + "this gate has no 'from' location");
 		
 		if (gate.getTo() != null)
-			sendMessage(ChatColor.GREEN + "'to' location:          " + ChatColor.YELLOW + "( " + gate.getTo().getBlockX() + " | " + gate.getTo().getBlockY() + " | " + gate.getTo().getBlockZ() + " )");
+			sendMessage(ChatColor.GREEN + "'to' location:          " + ChatColor.YELLOW + "( " + gate.getTo().getBlockX() + " | " + gate.getTo().getBlockY() + " | " + gate.getTo().getBlockZ() + " ) in " + gate.getTo().getWorld().getName());
 		else
 			sendMessage(ChatColor.GREEN + "this gate has no 'to' location");
 		
