fc5752f8401981ab110b18f0fe8ec88c2b8eab39
Tobias Ottenweller <mail@ottenweller.net>
2012-05-16 22:45:08 +0200

    refactoring of command close

--- src/org/mcteam/ancientgates/commands/CommandClose.java
+++ src/org/mcteam/ancientgates/commands/CommandClose.java
@@ -1,30 +1,24 @@
 package org.mcteam.ancientgates.commands;
 
-import org.bukkit.command.CommandSender;
+import org.mcteam.ancientgates.Plugin;
+
 
 public class CommandClose extends BaseCommand 
 {
 	public CommandClose()
 	{
 		aliases.add("close");
-		
 		requiredParameters.add("id");		
-		
 		helpDescription = "Close that gate";
+		requiredPermission = Plugin.permissionManage;
 	}
+	
 	
 	@Override
 	public void perform() 
 	{
 		gate.setOpen(false);
 		sendMessage("The gate was closed.");
-	}
-	
-	
-	@Override
-	public boolean hasPermission(CommandSender sender) 
-	{
-		return sender.hasPermission(permissionManage);
 	}
 }
 
