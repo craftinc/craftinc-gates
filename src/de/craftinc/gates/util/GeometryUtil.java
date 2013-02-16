f09b90f0a695ef68bd3dac62ff08c435b42e8f90
Tobias Ottenweller <mail@ottenweller.net>
2013-02-16 13:21:21 +0100

    Code style applied.

--- src/de/craftinc/gates/util/GeometryUtil.java
+++ src/de/craftinc/gates/util/GeometryUtil.java
@@ -5,11 +5,12 @@ import org.bukkit.Location;
 public class GeometryUtil {
 	
 	// How long between two locations?
-	public static double distanceBetweenLocations(Location location1, Location location2) {
+	public static double distanceBetweenLocations(Location location1, Location location2) 
+	{
 		double X = location1.getX() - location2.getX();
 		double Y = location1.getY() - location2.getY();
 		double Z = location1.getZ() - location2.getZ();
-		return Math.sqrt(X*X+Y*Y+Z*Z);
+		
+		return Math.sqrt(X*X + Y*Y + Z*Z);
 	}
-	
 }
