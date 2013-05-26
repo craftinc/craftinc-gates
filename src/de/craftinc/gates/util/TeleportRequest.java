093778f2fb0249ea53669d2d4d8b01dd9789fedb
Tobias Ottenweller <mail@ottenweller.net>
2013-05-26 14:52:15 +0200

    Added missing TeleportRequest class.

--- src/de/craftinc/gates/util/TeleportRequest.java
+++ src/de/craftinc/gates/util/TeleportRequest.java
@@ -1,11 +1,37 @@
 package de.craftinc.gates.util;
 
-/**
- * Created with IntelliJ IDEA.
- * User: tobi
- * Date: 26.05.13
- * Time: 14:28
- * To change this template use File | Settings | File Templates.
- */
-public class TeleportRequest {
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class TeleportRequest
+{
+    private Player player;
+    private Location destination;
+
+
+    public TeleportRequest(Player player, Location destination)
+    {
+        if (player == null) {
+            throw new IllegalArgumentException("Player must not be null!");
+        }
+
+        if (destination == null) {
+            throw new IllegalArgumentException("Destination must not be null");
+        }
+
+        this.player = player;
+        this.destination = destination;
+    }
+
+
+    public Player getPlayer()
+    {
+        return player;
+    }
+
+
+    public Location getDestination()
+    {
+        return destination;
+    }
 }
