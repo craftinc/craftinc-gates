8e860e90c6e242fff2cbb5ca555c1675b0e4cda1
Tobias Ottenweller <mail@ottenweller.net>
2013-05-26 12:36:43 +0200

    code style.

--- src/de/craftinc/gates/commands/CommandInfo.java
+++ src/de/craftinc/gates/commands/CommandInfo.java
@@ -47,12 +47,16 @@ public class CommandInfo extends BaseCommand
 		sendMessage(openHiddenMessage);
 		
 		if (gate.getLocation() != null)
-			sendMessage(ChatColor.DARK_AQUA + "from:  " + ChatColor.AQUA + "( " + (int)gate.getLocation().getX() + " | " + (int)gate.getLocation().getY() + " | " + (int)gate.getLocation().getZ() + " ) in " + gate.getLocation().getWorld().getName());
+			sendMessage(ChatColor.DARK_AQUA + "from:  " + ChatColor.AQUA + "( " + (int)gate.getLocation().getX() +
+                        " | " + (int)gate.getLocation().getY() + " | " + (int)gate.getLocation().getZ() + " ) in " +
+                        gate.getLocation().getWorld().getName());
 		else
 			sendMessage(ChatColor.DARK_AQUA + "NOTE: this gate has no location");
 		
 		if (gate.getExit() != null)
-			sendMessage(ChatColor.DARK_AQUA + "to:     " + ChatColor.AQUA + "( " + (int)gate.getExit().getX() + " | " + (int)gate.getExit().getY() + " | " + (int)gate.getExit().getZ() + " ) in " + gate.getExit().getWorld().getName());
+			sendMessage(ChatColor.DARK_AQUA + "to:     " + ChatColor.AQUA + "( " + (int)gate.getExit().getX() + " | "
+                        + (int)gate.getExit().getY() + " | " + (int)gate.getExit().getZ() + " ) in " +
+                        gate.getExit().getWorld().getName());
 		else
 			sendMessage(ChatColor.DARK_AQUA + "NOTE: this gate has no exit");
 	}
