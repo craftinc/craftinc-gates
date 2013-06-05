1968ccaaebf2767b49186010ebd7ebfa9eda71c0
Tobias Ottenweller <mail@ottenweller.net>
2013-06-05 13:00:29 +0200

    Added an authors file. Added a license section the each source file.

--- src/de/craftinc/gates/Plugin.java
+++ src/de/craftinc/gates/Plugin.java
@@ -1,3 +1,19 @@
+/*  Craft Inc. Gates
+    Copyright (C) 2011-2013 Craft Inc. Gates Team (see AUTHORS.txt)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program (LGPLv3).  If not, see <http://www.gnu.org/licenses/>.
+*/
 package de.craftinc.gates;
 
 import java.util.*;
@@ -11,7 +27,6 @@ import net.milkbowl.vault.permission.Permission;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
-import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
