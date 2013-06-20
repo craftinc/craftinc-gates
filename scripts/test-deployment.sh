931cd1a5200049969d714987ca37d86c70b24e09
Tobias Ottenweller <mail@ottenweller.net>
2013-06-20 22:35:46 +0200

    Updated the build scripts. Bumped version to 2.2.0-beta.

--- scripts/test-deployment.sh
+++ scripts/test-deployment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT_DIR="$(dirname "$0")"
 
 DEVELOPER=$(whoami)
 
@@ -14,18 +14,15 @@ else
 	START_STOP_SCRIPT="$SCRIPT_DIR/minecraft.sh"
 fi
 
-
+# TODO: This is a bad solution! Maven should write necessary information into an extra file.
+ARTIFACT_ID="$(grep -C5 '<groupId>de.craftinc' "$SCRIPT_DIR/../pom.xml" | grep '<name>' | sed 's/[ \t]*<name>//g' | sed 's/<\/name>[ \t]*//g')"
 
 # TODO: This is a bad solution! Maven should write necessary information into an extra file.
-ARTIFACT_ID="$(grep -C3 '<groupId>de.craftinc' "$SCRIPT_DIR/../pom.xml" | grep '<artifactId>' | sed 's/[ \t]*<artifactId>//g' | sed 's/<\/artifactId>[ \t]*//g')"
-
-# TODO: This is a bad solution! Maven should write necessary information into an extra file.
-VERSION="$(grep -C3 '<groupId>de.craftinc' "$SCRIPT_DIR/../pom.xml" | grep '<version>' | sed 's/[ \t]*<version>//g' | sed 's/<\/version>[ \t]*//g')"
-
+VERSION="$(grep -C5 '<groupId>de.craftinc' "$SCRIPT_DIR/../pom.xml" | grep '<version>' | sed 's/[ \t]*<version>//g' | sed 's/<\/version>[ \t]*//g')"
 
 mkdir -p "$PLUGIN_DIR"
 
-cp "$SCRIPT_DIR/../target/$ARTIFACT_ID-$VERSION".jar "$PLUGIN_DIR/"
+cp "$SCRIPT_DIR/../target/$ARTIFACT_ID $VERSION".jar "$PLUGIN_DIR/"
 
 echo -e "ddidderr\nmice_on_drugs\nMochaccino\nbeuth_el_max" > "$BUKKIT_DIR/ops.txt"
 
